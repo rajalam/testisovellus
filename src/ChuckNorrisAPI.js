0c38b31d5b62af5a2fc23ffa130be7f4e1b5c4ca
rajalam <mikko.rajala@iki.fi>
2022-10-24 15:40:18 +0300

    Tehtava1 valmis. Hakee vitsin 1x/click. Tests->ok

--- src/ChuckNorrisAPI.js
+++ src/ChuckNorrisAPI.js
@@ -7,39 +7,56 @@ function reducer(state, action) {
     switch (action.type) {
         case 'VITSIN_NOUTO_ALOITETTU':
             console.log("vitsin nouto aloitettu")
-            return { ...state, vitsinNoutoAloitettu: true }
+            return { ...state, vitsinNoutoAloitettu: true, haeUuttaDataa: false }
         case 'VITSI_NOUDETTU':
             console.log("vitsi noudettu")
-            return { ...state, vitsi: action.payload /* ,kategorioidenNoutoAloitettu:false */ }
+            return { ...state, vitsi: action.payload, haeUuttaDataa: false /* ,kategorioidenNoutoAloitettu:false */ }
         case 'VITSIN_NOUTO_EPÄONNISTUI':
             console.log("datan nouto epäonnistui")
             return { ...state, vitsinNoutoEpäonnistui: true, vitsinNoutoAloitettu: false }
+        case 'NOUDA_VITSI':
+            console.log("näytetään vitsi", action.payload)
+            //haeDataa();
+            return { ...state, haeUuttaDataa: true }
         case 'YYYY':
         default:
             throw new Error("Action.type kentän arvoa ei tunnistettu");
     }
 }
 function AppChuckNorris() {
-    const [appData, dispatch] = useReducer(reducer, { vitsi: "", 
-        vitsinNoutoAloitettu: false, 
-        vitsinNoutoEpäonnistui: false });
+    const [appData, dispatch] = useReducer(reducer, {
+        vitsi: "",
+        vitsinNoutoAloitettu: false,
+        vitsinNoutoEpäonnistui: false,
+        haeUuttaDataa: false
+    });
 
     useEffect(() => {
         async function haeDataa() {
             //let result = await axios('https://api.huuto.net/1.1/categories');
+
+
             try {
                 dispatch({ type: 'VITSIN_NOUTO_ALOITETTU' })
+
+                //await axios onnistumista on vaikea testailla Firefoxilla Tiedosto->Työskentele yhteydettömässä
+                //tilassa, koska selain tuottaa vaan edellisen tuloksen tuolla haulla ilmeisesti
+                //selaimen välimuistista, enkä saa Firefoxia heittämään poikkeusta, vaikka axios yhteyttä
+                //APIin ei löytyisikään
                 let result = await axios('https://api.chucknorris.io/jokes/random');
                 dispatch({ type: 'VITSI_NOUDETTU', payload: result.data.value })
                 console.log(result.data.value)
             } catch (error) {
+
                 console.log("Tuli muuten hitonmoinen ongelma:", error)
                 dispatch({ type: 'VITSIN_NOUTO_EPÄONNISTUI' })
 
             }
         }
-        haeDataa();
-    }, []);
+        if (appData.haeUuttaDataa) { //haetaan vain kun uutta tietoa tilattu
+            haeDataa();
+        }
+    }, [appData.haeUuttaDataa]);
 
     return (
         <div>
@@ -51,6 +68,13 @@ function AppChuckNorris() {
 
             <div>{appData.vitsinNoutoEpäonnistui && "Vitsin nouto epäonnistui"}</div>
 
+            <div>
+                <h6><input type="button" onClick={(event) => {
+                    dispatch({
+                        type: 'NOUDA_VITSI'
+                    })
+                }}
+                    value="Hae vitsi!" /></h6></div>
         </div>
     );
 }
